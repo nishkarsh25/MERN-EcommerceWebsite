f8f89459c78d3a3fe92182e68a4285a6cd21dbbb
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-16 19:11:28 +0530

    Importing Middleware Functions:
    The lines const auth = require('../middleware/auth') and const authAdmin = require('../middleware/authAdmin') import middleware functions (auth and authAdmin) responsible for user authentication and authorization.

--- server/routes/categoryRouter.js
+++ server/routes/categoryRouter.js
@@ -1,1 +1,5 @@
 const router = require('express').Router()
+const categoryCtrl = require('../controllers/categoryCtrl')
+const auth = require('../middleware/auth')
+const authAdmin = require('../middleware/authAdmin')
+
