18e0d94267849d4768b45ca4572122c395b4b98b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-16 08:46:28 +0530

    Summary The code handles user registration and refresh token operations. It ensures secure password storage using bcrypt. It creates and manages JWTs for authentication. It uses cookies to store refresh tokens securely. It includes error handling to manage potential issues during these processes.

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -3,69 +3,69 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 
 const userCtrl = {
-    register: async(req,res) => {
-        try{
-            const {name,email,password} = req.body;
+    register: async (req, res) => {
+        try {
+            const { name, email, password } = req.body;
 
-            const user = await Users.findOne({email})
-            if(user) return res.status(400).json({msg:"Email Already Registered"})
+            const user = await Users.findOne({ email })
+            if (user) return res.status(400).json({ msg: "Email Already Registered" })
 
-            if(password.length < 6)
-            return res.status(400).json({msg:"Password is at least 6 character"})
+            if (password.length < 6)
+                return res.status(400).json({ msg: "Password is at least 6 character" })
 
             //Password Encryption
-            const passwordHash = await bcrypt.hash(password,10)
+            const passwordHash = await bcrypt.hash(password, 10)
 
             const newUser = new Users({
-                name,email,password:passwordHash
+                name, email, password: passwordHash
             })
 
             //Save mongodb
             await newUser.save()
 
             //create jwt to authenticate
-            const accesstoken = createAccessToken({id:newUser._id})
-            const refreshtoken = createRefreshToken({id:newUser._id})
+            const accesstoken = createAccessToken({ id: newUser._id })
+            const refreshtoken = createRefreshToken({ id: newUser._id })
 
-            res.cookie('refreshtoken', refreshtoken,{
-                httpOnly:true,
-                path:'/user/refresh_token'
+            res.cookie('refreshtoken', refreshtoken, {
+                httpOnly: true,
+                path: '/user/refresh_token'
             })
 
-            res.json({accesstoken})
+            res.json({ accesstoken })
 
         }
-        catch(err){
-            return res.status(500).json({msg:err.message})
+        catch (err) {
+            return res.status(500).json({ msg: err.message })
         }
     },
-    refreshtoken: async(req,res) => {
+    refreshtoken: async (req, res) => {
 
-        try{
+        try {
             const rf_token = req.cookies.refreshtoken;
 
-            if(!rf_token) return res.status(400).json({msg:"Please Login or Registers"});
-    
-            jwt.verify(rf_token,process.env.REFRESH_TOKEN_SECRET,(err,user) => {
-                if(err) return res.status(400).json({msg:"Please Login or Register"})
-                const accesstoken = createAccessToken({id:user.id})
-            res.json({accesstoken})
-            })
-    
-        }
-        catch(err){
-return res.status(500).json({msg:err.message})
-        }
-       
+            if (!rf_token) return res.status(400).json({ msg: "Please Login or Registers" });
 
-    }   
+            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+                if (err) return res.status(400).json({ msg: "Please Login or Register" })
+                const accesstoken = createAccessToken({ id: user.id })
+                res.json({ accesstoken })
+            })
+
+        }
+        catch (err) {
+            return res.status(500).json({ msg: err.message })
+        }
+
+
+    }
 }
 
 const createAccessToken = (payload) => {
-    return jwt.sign(payload,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1d'})
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
 }
 const createRefreshToken = (payload) => {
-    return jwt.sign(payload,process.env.REFRESH_TOKEN_SECRET,{expiresIn:'7d'})
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
 }
 
 module.exports = userCtrl
