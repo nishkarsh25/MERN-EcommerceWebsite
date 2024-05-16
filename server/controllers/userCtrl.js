e05684785b58b682e3f055cb580e607ab75702ae
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-16 08:44:57 +0530

    Helper Functions createAccessToken Function Purpose: Creates a new JWT access token. Payload: Includes user information, such as the user ID. Secret: Uses ACCESS_TOKEN_SECRET from the environment variables. Expiration: Sets the token to expire in 1 day. createRefreshToken Function Purpose: Creates a new JWT refresh token. Payload: Includes user information, such as the user ID. Secret: Uses REFRESH_TOKEN_SECRET from the environment variables. Expiration: Sets the token to expire in 7 days.

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -6,3 +6,10 @@ const userCtrl = {
     
 }
 
+const createAccessToken = (payload) => {
+    return jwt.sign(payload,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1d'})
+}
+const createRefreshToken = (payload) => {
+    return jwt.sign(payload,process.env.REFRESH_TOKEN_SECRET,{expiresIn:'7d'})
+}
+
