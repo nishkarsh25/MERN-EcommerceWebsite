fdbdaad9c2316e817016f8d8b001f9bb449e2a96
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-16 11:04:47 +0530

    Token Verification:
    After retrieving the token, the middleware checks if the token exists. If it doesn't, indicating that the user is not authenticated, it returns a 400 status response with the message "Invalid Authentication".
    If the token exists, the middleware proceeds to verify its authenticity using jwt.verify().
    It calls jwt.verify() with the token, the secret key (process.env.ACCESS_TOKEN_SECRET), and a callback function to handle the verification result.
    If the token is successfully verified (i.e., the signature matches and it hasn't expired), the callback function is called with the decoded payload (user) as an argument.
    If there's an error during verification (e.g., invalid token or signature), the callback function is called with the error object as the first argument.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,14 @@ const auth = (req,res,next) => {
     try{
         const token = req.header("Authorization")
         console.log(token)
-        
+        if(!token) return res.status(400).json({msg:"Invalid Authentication"})
+
+        jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+            if(err) return res.status(400).json({msg:"Invalid Authentication"})
+
+            
+            
+        })
     }catch(err){
         
     }
