eee1d9e3c0bfc9a6574e10e243e6c6781ff37137
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-16 11:12:15 +0530

    Response Handling:
    If a user document is found in the database, the function sends a JSON response containing the user object retrieved from the database.
    If no user document is found (i.e., the user variable is null), the function returns a 400 status response with the message "User Not Found".

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -95,7 +95,8 @@ return res.status(500).json({msg:err.message})
         try{
             const user = await Users.findById(req.user.id).select('-password')
 
-            
+            if(!user) return res.status(400).json({msg:"User Not Found"})
+            res.json(user)
         }
         catch(err){
             
