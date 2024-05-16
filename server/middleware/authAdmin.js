e0a4653585b02dedca57f72c669ab5acd87e1088
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-16 11:16:54 +0530

    User Query:
    Inside the middleware function, it queries the database to find the user associated with the authenticated user's ID (req.user.id).
    It uses Users.findOne() to search for a user document in the database that matches the provided query criteria (in this case, the user's ID).
    The await keyword is used to wait for the database query to complete asynchronously, allowing the function to proceed once the user document is retrieved.

--- server/middleware/authAdmin.js
+++ server/middleware/authAdmin.js
@@ -2,6 +2,10 @@ const Users = require('../models/userModel')
 
 const authAdmin = async(req,res,next) => {
     try{
+        const user = await Users.findOne({
+            _id : req.user.id
+        })
+
         
     }
     catch(err){
