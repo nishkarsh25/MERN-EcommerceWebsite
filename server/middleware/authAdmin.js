03a2223d2edae040e2b8d8be2464a3ac831d7ec2
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-16 11:17:34 +0530

    Passing Control to Next Middleware/Route Handler:
    If the user has an admin role (i.e., role is not equal to 0), the middleware calls the next() function to pass control to the next middleware function or route handler in the request handling chain.
    This allows subsequent middleware functions or route handlers to be invoked, enabling further processing of the request.

--- server/middleware/authAdmin.js
+++ server/middleware/authAdmin.js
@@ -9,7 +9,7 @@ const authAdmin = async(req,res,next) => {
         if(user.role === 0)
         return res.status(400).json({msg:"Admin Resources Access Denied"})
 
-        
+        next()
     }
     catch(err){
         
