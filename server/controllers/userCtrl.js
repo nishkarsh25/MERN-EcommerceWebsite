9e37d9f6c804ea38ee4fa16849079a99cfa81431
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-16 08:45:48 +0530

    register Method Extracting Data from Request:
    Extracts name, email, and password from the request body.
    Checking for Existing User:
    
    Queries the Users collection to see if a user with the provided email already exists.
    If a user is found, it returns a response indicating that the email is already registered.
    Validating Password Length:
    
    Checks if the password length is at least 6 characters.
    If the password is too short, it returns a response indicating the requirement.
    Password Encryption:
    
    Uses bcrypt to hash the user's password with a salt round of 10. This ensures the password is stored securely.
    Creating New User:
    
    Creates a new user object with the provided name, email, and the hashed password.
    Saves the new user to the MongoDB database.
    Creating JWTs:
    
    Generates an access token using the createAccessToken function.
    Generates a refresh token using the createRefreshToken function.
    Setting Refresh Token in Cookies:
    
    Sets the refresh token in an HTTP-only cookie to ensure it’s secure and not accessible via JavaScript.
    Sending Response:
    
    Sends a response with the access token to the client.
    Error Handling:
    
    Catches any errors that occur during the process and returns a 500 status with the error message.

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -3,7 +3,42 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 
 const userCtrl = {
-    
+    register: async(req,res) => {
+        try{
+            const {name,email,password} = req.body;
+
+            const user = await Users.findOne({email})
+            if(user) return res.status(400).json({msg:"Email Already Registered"})
+
+            if(password.length < 6)
+            return res.status(400).json({msg:"Password is at least 6 character"})
+
+            //Password Encryption
+            const passwordHash = await bcrypt.hash(password,10)
+
+            const newUser = new Users({
+                name,email,password:passwordHash
+            })
+
+            //Save mongodb
+            await newUser.save()
+
+            //create jwt to authenticate
+            const accesstoken = createAccessToken({id:newUser._id})
+            const refreshtoken = createRefreshToken({id:newUser._id})
+
+            res.cookie('refreshtoken', refreshtoken,{
+                httpOnly:true,
+                path:'/user/refresh_token'
+            })
+
+            res.json({accesstoken})
+
+        }
+        catch(err){
+            return res.status(500).json({msg:err.message})
+        }
+    }
 }
 
 const createAccessToken = (payload) => {
