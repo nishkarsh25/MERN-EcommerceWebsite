c6814532ea77f490a7e591a389e39f85ee24d5cd
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-16 11:04:26 +0530

    Token Retrieval:
    Inside the middleware function, it attempts to retrieve the JWT from the request header.
    It uses req.header("Authorization") to access the 'Authorization' header of the incoming request, which typically contains the JWT.
    The retrieved token is stored in the token variable.

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,8 @@ const jwt = require('jsonwebtoken')
 
 const auth = (req,res,next) => {
     try{
+        const token = req.header("Authorization")
+        console.log(token)
         
     }catch(err){
         
