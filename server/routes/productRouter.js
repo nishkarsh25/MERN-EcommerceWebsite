5d4d0f969bc5a4a4a7e96c43553449014bca79cb
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-17 09:56:44 +0530

    router.route('/products'): This defines a route for handling requests related to products. The route is specified as /products.
    .get(productCtrl.getProducts): This middleware function handles HTTP GET requests to the /products route. It invokes the getProducts function from the productCtrl object, which retrieves a list of products.
    
    .post(productCtrl.createProducts): This middleware function handles HTTP POST requests to the /products route. It invokes the createProducts function from the productCtrl object, which creates a new product.

--- server/routes/productRouter.js
+++ server/routes/productRouter.js
@@ -1,3 +1,8 @@
 const router = require('express').Router()
 const productCtrl = require('../controllers/productCtrl')
 
+router.route('/products')
+.get(productCtrl.getProducts)
+.post(productCtrl.createProducts)
+
+
