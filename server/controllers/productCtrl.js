f9fd6f5718a0d63e1d0a3c4dbe939190791d9c1d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-17 09:25:28 +0530

    updateProduct:
    This method handles the update of a product by its ID.
    It extracts the updated product details from the request body (req.body).
    It checks if images are provided and returns a 500 status with an error message if not.
    It uses the findOneAndUpdate() method provided by Mongoose to find the product by its ID and update its details with the provided data.
    Upon successful update, it sends a JSON response confirming the update of the product.
    Overall, the productCtrl object encapsulates the logic for handling CRUD (Create, Read, Update, Delete) operations related to products in the application. Each method corresponds to a specific CRUD operation and interacts with the database using Mongoose methods. Additionally, it handles error cases and sends appropriate responses to the client.

--- server/controllers/productCtrl.js
+++ server/controllers/productCtrl.js
@@ -99,6 +99,23 @@ const productCtrl = {
         }catch(err){
             return res.status(500).json({msg:err.message})
         }
+    },
+    updateProduct:async(req,res) => {
+        try{
+            const {title,price,description,content,images,category} = req.body;
+
+            if(!images) return res.status(500).json({msg:"No Image Upload"})
+
+            await Products.findOneAndUpdate({_id:req.params.id},{
+                title:title.toLowerCase(),price,description,content,images,category
+            })
+
+            res.json({msg:"Updated a Product"})
+        }
+        catch(err){
+            return res.status(500).json({msg:err.message})
+        }
     }
 }
 
+module.exports = productCtrl
