593ee22f74bc49643990fd6d8ec37ed6f3ec9337
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-17 09:24:52 +0530

    createProducts:
    This method handles the creation of a new product.
    It extracts the necessary data (product details) from the request body (req.body).
    It checks if images are provided and returns a 400 status with an error message if not.
    It queries the database to check if a product with the same product_id already exists. If found, it returns a 400 status with an error message.
    If the product does not exist, it creates a new instance of the Products model with the provided data and saves it to the database.
    Finally, it sends a JSON response confirming the creation of the product.

--- server/controllers/productCtrl.js
+++ server/controllers/productCtrl.js
@@ -68,6 +68,29 @@ const productCtrl = {
         catch(err){
             return res.status(500).json({msg:err.message})
         }
+    },
+    createProducts:async(req,res) => {
+        try{
+            const {product_id,title,price,description,content,images,category} = req.body
+
+            if(!images) return res.status(400).json({msg:"No Image Upload"})
+
+            const product = await Products.findOne({product_id})
+
+            if(product)
+            return res.status(400).json({msg:"This product already exists"})
+
+            const newProduct = new Products({
+                product_id,title : title.toLowerCase(),price,description,content,images,category
+            })
+
+            await newProduct.save();
+
+            res.json({msg:"Create a product"})
+        }
+        catch(err){
+            return res.status(500).json({msg:err.message})
+        }
     }
 }
 
