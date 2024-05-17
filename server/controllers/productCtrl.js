0d6f7b5fd22f9136f281d986df1765e7e7b3a743
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-17 09:17:22 +0530

    Pagination Method:
    pagination() { ... }: This method handles pagination by skipping a certain number of documents and limiting the number of documents returned in a single page.
    It extracts the page and limit query string parameters from queryString, converting them to numbers using the * 1 operation. If these parameters are not provided, it defaults to page 1 and a limit of 9 documents per page.
    It calculates the number of documents to skip (skip) based on the page number and limit.
    It updates the query property of the APIfeatures instance to apply pagination to the query.
    Each method in the APIfeatures class modifies the query property of the instance based on the provided query string parameters, allowing for flexible and dynamic MongoDB queries for filtering, sorting, and pagination.

--- server/controllers/productCtrl.js
+++ server/controllers/productCtrl.js
@@ -40,6 +40,17 @@ class APIfeatures{
         return this
     }
 
-   
+    pagination(){
+
+        const page = this.queryString.page * 1 || 1;
+
+        const limit =  this.queryString.limit * 1 || 9;
+
+        const skip = (page-1) * limit;
+
+        this.query = this.query.skip(skip).limit(limit);
+
+        return this;
+    }
 }
 
