8e7070da68e7e67a7cf2961d2ad860d00a2e7dd9
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-17 09:16:25 +0530

    Constructor:
    constructor(query, queryString) { ... }: The constructor method is a special method that is automatically called when you create a new instance of the APIfeatures class. It accepts two parameters: query and queryString.
    query: This parameter represents the MongoDB query object. It typically contains conditions and options for querying the database.
    queryString: This parameter represents the query string parameters received from the HTTP request. In Express.js, it's commonly accessed via req.query and contains parameters for filtering, sorting, pagination, etc.

--- server/controllers/productCtrl.js
+++ server/controllers/productCtrl.js
@@ -5,6 +5,11 @@ const Products = require('../models/productModel')
 //Filter,sorting and pagination
 
 class APIfeatures{
+    constructor(query,queryString){
+        this.query = query;
+        this.queryString = queryString
+    }
+
     
 }
 
