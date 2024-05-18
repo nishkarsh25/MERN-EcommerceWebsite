6008d52ac0f0386090f1df666988946c2b39891c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-18 06:16:59 +0530

    getProducts Function:
    Purpose: To fetch the list of products from the server.
    Implementation:
    Uses axios.get to send a GET request to the specified URL (http://localhost:5000/api/products).
    Awaits the response from the server.
    Logs the response to the console for debugging purposes.
    Sets the products state with the data fetched from the server (res.data.products).

--- client/src/api/ProductAPI.jsx
+++ client/src/api/ProductAPI.jsx
@@ -5,6 +5,12 @@ const ProductAPI = () => {
 
     const [products,setProducts]=useState([])
 
+    const getProducts = async()=> {
+        const res = await axios.get('http://localhost:5000/api/products')
+        console.log(res)
+        setProducts(res.data.products)
+    }
+
     
 }
 
