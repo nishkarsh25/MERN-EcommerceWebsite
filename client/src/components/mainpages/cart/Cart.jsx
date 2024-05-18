c0f62567cc7502ecc5ca3f6b3d39027d4b9edc03
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-18 13:15:38 +0530

    Total Price and Total Items Calculation:
    totalPrice: It calculates the total price of all items in the cart. It uses the reduce method to iterate over each product in the cart, multiplying its price by its quantity, and summing up the results.
    totalItems: It calculates the total number of items in the cart. Similar to totalPrice, it uses the reduce method to sum up the quantities of all products in the cart.

--- client/src/components/mainpages/cart/Cart.jsx
+++ client/src/components/mainpages/cart/Cart.jsx
@@ -5,7 +5,9 @@ const Cart = () => {
   const state = useContext(GlobalState);
   const [cart, setCart] = state.userAPI.cart;
 
-  
+  // Calculate total price and total number of items in cart
+  const totalPrice = cart.reduce((acc, product) => acc + product.price * product.quantity, 0);
+  const totalItems = cart.reduce((acc, product) => acc + product.quantity, 0);
 
   
 
