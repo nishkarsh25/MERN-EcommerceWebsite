98812759c01f2353904251e5ea9c9c2adb41847e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-18 13:15:28 +0530

    In this section of the code:
    javascript
    Copy code
    const state = useContext(GlobalState);
    const [cart, setCart] = state.userAPI.cart;
    UseContext Hook:
    
    useContext(GlobalState): This hook is used to access the GlobalState context object that was imported earlier. It allows the component to consume the state values provided by the context.
    Destructuring Cart State:
    
    [cart, setCart] = state.userAPI.cart: This line destructures the cart state value and its associated setter function (setCart) from the userAPI object within the GlobalState.
    cart: It represents the current cart items stored in the state.
    setCart: It is a function used to update the cart state.

--- client/src/components/mainpages/cart/Cart.jsx
+++ client/src/components/mainpages/cart/Cart.jsx
@@ -2,7 +2,8 @@ import React, { useContext, useState } from 'react';
 import { GlobalState } from '../../../GlobalState';
 
 const Cart = () => {
-  
+  const state = useContext(GlobalState);
+  const [cart, setCart] = state.userAPI.cart;
 
   
 
