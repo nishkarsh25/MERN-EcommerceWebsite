4d0221d5b4a198636a602cf8158cddc8848f63d0
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-18 12:20:55 +0530

    Rendering Products:
    The component returns a JSX structure that renders a grid layout using Tailwind CSS classes.
    It iterates over the products array using the map method to render each product.
    For each product, it renders a ProductList component passing the following props:
    key: Unique identifier for React reconciliation.
    product: The product object itself.
    isAdmin: A boolean indicating whether the user is an admin or not. This is used to conditionally render certain features based on user role.

--- client/src/components/mainpages/products/Product.jsx
+++ client/src/components/mainpages/products/Product.jsx
@@ -2,6 +2,20 @@ import React, { useContext } from 'react'
 import { GlobalState } from '../../../GlobalState'
 import ProductList from '../utils/ProductLists/ProductList'
 
+const Product = () => {
+  const state = useContext(GlobalState)
+  const [products] = state.productsAPI.products
+  const [isAdmin] = state.userAPI.isAdmin
 
+  return (
+    <div className="container mx-auto mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">      
+      {
+        products.map(product => {
+          return <ProductList key={product._id} product={product} isAdmin={isAdmin}/>
+        })
+      }      
+    </div>
+  )
+}
 
 
