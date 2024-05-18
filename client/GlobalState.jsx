ac640a549ec93dfd49953343a3f272f4ed9ffac0
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-18 06:24:23 +0530

    Imports createContext and useEffect from React:
    createContext: This function is used to create a Context object. When React renders a component that subscribes to this Context object, it will read the current context value from the closest matching Provider above it in the tree.
    useEffect: This hook lets you perform side effects in function components. It runs after the component mounts and after every update, depending on the dependency array provided.
    useState from React:
    
    useState: A hook that lets you add state to function components. It returns an array with the current state value and a function to update it.
    axios:
    
    axios: A promise-based HTTP client for making requests to servers. It's used here to fetch data from an API.
    ProductAPI and UserAPI:
    
    ProductAPI: A custom hook/function that fetches product data from an API.
    UserAPI: A custom hook/function that fetches user data and manages user-related state and actions, taking a token as an argument for authenticated requests.

---
+++ client/GlobalState.jsx
@@ -0,0 +1,8 @@
+import { createContext, useEffect } from "react";
+import ProductAPI from "./api/ProductAPI";
+import { useState } from "react";
+import axios from "axios";
+import UserAPI from "./api/UserAPI";
+
+
+
