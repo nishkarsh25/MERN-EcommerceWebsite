0ae87f7e15938976354255107a0b070bbb883252
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-05-18 06:21:53 +0530

    UserAPI Component State Declarations:
    isLogged: A state variable that indicates whether the user is logged in. Initially set to false.
    setIsLogged: A function to update the isLogged state.
    isAdmin: A state variable that indicates whether the logged-in user is an admin. Initially set to false.
    setIsAdmin: A function to update the isAdmin state.
    cart: A state variable that holds the array of items in the user's cart. Initially set to an empty array.
    setCart: A function to update the cart state.

--- client/src/api/UserAPI.jsx
+++ client/src/api/UserAPI.jsx
@@ -1,6 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
+const UserAPI = (token) => {
+    const [isLogged, setIsLogged] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [cart, setCart] = useState([]);
 
+    
+
+    
+
+    
+};
 
 
